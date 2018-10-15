0e3cfbc1d03f5804da49bf056a871d63d4dd0185
Tony Johnson <tony@tosp.net.au>
2018-10-15 15:48:39 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# minimal-react-scaffold
+
+A minimal React/Webpack/Babel scaffold
+
+1. git clone
+2. cd minimal-react-scaffold
+3. ./my-create-react-app
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return <div>I am a React App</div>
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
